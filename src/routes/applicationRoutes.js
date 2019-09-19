d3dd484b4fe8aedc70c7bb74db03a6c133c18f1d
Sai Bala Krishna Allamsetty <sbk888.sbk@gmail.com>
2019-09-19 10:14:22 +0530

    Testing save file

--- src/routes/applicationRoutes.js
+++ src/routes/applicationRoutes.js
@@ -6,13 +6,26 @@ const router = express.Router();
 
 const appController = require('../controllers/appController');
 
+router.post('/addApp', appController.addAppDetails);
 
-router.get('/showall', appController.retrieveAllApps);
+router.post('/uploadApp/upload', appController.uploadFile);
 
-router.get('/showHistory/:appName', appController.retrieveAppHistory)
+router.get('/uploadApp/upload', (req, res, next) => {
+    res.send('<form method="post" enctype="multipart/form-data" action="/appdetails/uploadApp/upload">    <input type="file" name="file">    <input type="submit" value="Submit"></form>');
+});
+
+
+
+
 router.get('/:appName', appController.retrieveApp);
 
-router.post('/addApp', appController.addAppDetails);
+router.get('/display/all', appController.retrieveAllApps);
+
+router.get('/display/history/:appName', appController.retrieveAppHistory);
+
+
+
+
 
 router.get('/', (req, res, next) => {
     res.redirect('/admin');
