8804a4e8cf90d9b1ac3eeb87997563130dfd2558
admin <sbk888.sbk@gmail.com>
2019-10-17 12:26:06 +0530

    modified user response with application List var

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -18,7 +18,7 @@ exports.validateUser = (req, res, next) => {
         if(!user){
             console.log("Email Id does not exists")
             response.message =  "Our records do not have your email! Please register"
-            return res.send(response)
+            
         } else {
             if(email === user.email){
                 console.log('Email Id exists')
@@ -27,18 +27,19 @@ exports.validateUser = (req, res, next) => {
                     response.status     = "valid"
                     response.userRole   = user.userRole;
                     response.message    = "Login Successfull" ;
-                    res.send(JSON.stringify(response));
+                    response.appsAccess = user.applicationList;
                 }
                 else{
                     response.message = "Invalid Password";
-                    res.send(JSON.stringify(response));
+                    
                 }
 
             } else{
                 response.message = "Invalid Email ID";
-                res.send(JSON.stringify(response));
+                
             }
         }
+        res.send(JSON.stringify(response));
 
 
     }
